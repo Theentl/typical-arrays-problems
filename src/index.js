2e879ce5c0e1ab2df7008d7b6e39ebf860c0ee3d
Azamat <53929580+Theentl@users.noreply.github.com>
2021-03-13 15:12:31 +0300

    init: create project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+  if (array.length > 0) {
+      return Math.min.apply(null, array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+   if (array.length > 0) {
+       return Math.max.apply(null, array);
+ }
 }
-
 exports.avg = function avg (array) {
-  return 0;
+  let i = 0;
+  let avrg = 0;
+  if (array === undefined || array.length === 0) return 0;
+  else {
+      while (i < array.length) {
+        avrg += array[i];
+        i++;
+      }
+      return avrg / array.length;
+    }
 }
